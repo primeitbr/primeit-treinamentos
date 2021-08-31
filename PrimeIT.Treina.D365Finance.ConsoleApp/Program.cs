76affddf00a131c4856f68f18072b2f9d05413cf
Moacir Jr <prezasoftwares@gmail.com>
2021-08-30 22:20:28 -0300

    Add project files.

---
+++ PrimeIT.Treina.D365Finance.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeIT.Treina.D365Finance.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (CreditLimitWeb creditLimitWeb = new CreditLimitWeb())
+            {
+                string amount = creditLimitWeb.getCustomerCreditLimit("https://primeit-treina.azurewebsites.net/api/limitcreditcalc?externalId=123");
+                Console.WriteLine(amount);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PrimeIT.Treina.D365Finance/CreditLimitWeb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeIT.Treina.D365Finance
+{
+    public class CreditLimitWeb : IDisposable
+    {
+        HttpClient client;
+
+        public CreditLimitWeb()
+        {
+            client = new HttpClient();
+        }
+
+        public void Dispose()
+        {
+            client = null;
+        }
+
+        public string getCustomerCreditLimit(string uri)
+        {
+            var responseTask = client.PostAsync(uri, null);
+            var response = responseTask.Result;
+            var amount = response.Content.ReadAsStringAsync();
+
+            return amount.Result;
+        }
+    }
+}
